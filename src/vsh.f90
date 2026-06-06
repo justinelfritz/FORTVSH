2bc640114263c9368336ce57f998cd50c06a7ce9
Justin Elfritz <justin.elfritz@gmail.com>
2026-06-05 22:10:48 -0400

    Issue #3: resolved residual issues with indentation

--- src/vsh.f90
+++ src/vsh.f90
@@ -73,49 +73,49 @@ CONTAINS
   END FUNCTION DDX_LEGENDRE
 
   FUNCTION ASSOC_LEGENDRE(L, K, X)
-      IMPLICIT NONE
-      INTEGER(KIND=i4), INTENT(IN) :: L, K
-      REAL(KIND=dp), INTENT(IN) :: X
-      INTEGER(KIND=i4) :: ABSK, I
-      REAL(KIND=dp) :: ASSOC_LEGENDRE
-      REAL(KIND=dp) :: Pmm, Pmm1, Plm, somx2
+  IMPLICIT NONE
+  INTEGER(KIND=i4), INTENT(IN) :: L, K
+  REAL(KIND=dp), INTENT(IN) :: X
+  INTEGER(KIND=i4) :: ABSK, I
+  REAL(KIND=dp) :: ASSOC_LEGENDRE
+  REAL(KIND=dp) :: Pmm, Pmm1, Plm, somx2
 
-      ABSK = ABS(K)
+  ABSK = ABS(K)
 
-      IF (ABSK > L .OR. ABS(X) > 1.0_dp) THEN
-          ASSOC_LEGENDRE = 0.0_dp
-          RETURN
-      END IF
+  IF (ABSK > L .OR. ABS(X) > 1.0_dp) THEN
+    ASSOC_LEGENDRE = 0.0_dp
+    RETURN
+  END IF
 
-      Pmm = 1.0_dp
-      IF (ABSK > 0) THEN
-          somx2 = sqrt((1.0_dp - X) * (1.0_dp + X))
-          DO I = 1, ABSK
-              Pmm = -Pmm * (2*I - 1) * somx2
-          END DO
-      END IF
+  Pmm = 1.0_dp
+  IF (ABSK > 0) THEN
+    somx2 = sqrt((1.0_dp - X) * (1.0_dp + X))
+    DO I = 1, ABSK
+      Pmm = -Pmm * (2*I - 1) * somx2
+    END DO
+  END IF
 
-      IF (L == ABSK) THEN
-          Plm = Pmm
-      ELSE
-          Pmm1 = X * (2 * ABSK + 1) * Pmm
-          IF (L == ABSK + 1) THEN
-              Plm = Pmm1
-          ELSE
-              DO I = ABSK + 2, L
-                  Plm = (X*(2*I-1)*Pmm1-(I+ABSK-1)*Pmm) / (I-ABSK)
-                  Pmm = Pmm1
-                  Pmm1 = Plm
-              END DO
-          END IF
-      END IF
+  IF (L == ABSK) THEN
+    Plm = Pmm
+  ELSE
+    Pmm1 = X * (2 * ABSK + 1) * Pmm
+    IF (L == ABSK + 1) THEN
+      Plm = Pmm1
+    ELSE
+      DO I = ABSK + 2, L
+        Plm = (X*(2*I-1)*Pmm1-(I+ABSK-1)*Pmm) / (I-ABSK)
+        Pmm = Pmm1
+        Pmm1 = Plm
+      END DO
+    END IF
+  END IF
 
-      IF (K < 0) THEN
-          Plm = ((-1.0_dp)**ABSK)* &
-                EXP(LOG_FACT(L-ABSK)-LOG_FACT(L+ABSK))*Plm
-      END IF
+  IF (K < 0) THEN
+    Plm = ((-1.0_dp)**ABSK)* &
+      EXP(LOG_FACT(L-ABSK)-LOG_FACT(L+ABSK))*Plm
+  END IF
 
-      ASSOC_LEGENDRE = Plm
+  ASSOC_LEGENDRE = Plm
   END FUNCTION ASSOC_LEGENDRE
 
 !     Compute P_l^k(x) and d/dx P_l^k(x) in a single upward recurrence.
@@ -176,11 +176,11 @@ CONTAINS
   END SUBROUTINE ASSOC_LEGENDRE_AND_DERIV
 
   FUNCTION DDX_ASSOC_LEGENDRE(L, K, X) RESULT(RES)
-      IMPLICIT NONE
-      INTEGER(KIND=i4), INTENT(IN) :: L, K
-      REAL(KIND=dp), INTENT(IN)    :: X
-      REAL(KIND=dp)                :: RES, PLK_UNUSED
-      CALL ASSOC_LEGENDRE_AND_DERIV(L, K, X, PLK_UNUSED, RES)
+  IMPLICIT NONE
+  INTEGER(KIND=i4), INTENT(IN) :: L, K
+  REAL(KIND=dp), INTENT(IN)    :: X
+  REAL(KIND=dp)                :: RES, PLK_UNUSED
+  CALL ASSOC_LEGENDRE_AND_DERIV(L, K, X, PLK_UNUSED, RES)
   END FUNCTION DDX_ASSOC_LEGENDRE
 
 !     Compute scalar spherical harmonic Y_l^k(theta,phi) at 0<=theta<=pi, 0<=phi<=2pi
@@ -463,15 +463,15 @@ CONTAINS
 
 !     Compute log(factorial()) for stability  
   FUNCTION LOG_FACT(N)
-      IMPLICIT NONE
-      INTEGER(KIND=i4), INTENT(IN) :: N
-      REAL(KIND=dp) :: LOG_FACT
-      IF (N <= 1) THEN
-          LOG_FACT = 0.0_dp
-      ELSE
-          ! ln(n!) = ln(Gamma(n+1))
-          LOG_FACT = LOG_GAMMA(REAL(N + 1, KIND=dp))
-      END IF
+  IMPLICIT NONE
+  INTEGER(KIND=i4), INTENT(IN) :: N
+  REAL(KIND=dp) :: LOG_FACT
+  IF (N <= 1) THEN
+    LOG_FACT = 0.0_dp
+  ELSE
+    ! ln(n!) = ln(Gamma(n+1))
+    LOG_FACT = LOG_GAMMA(REAL(N + 1, KIND=dp))
+  END IF
   END FUNCTION LOG_FACT
 
 !     Compute Gauss-Legendre quadrature nodes and weights for exact
@@ -596,16 +596,16 @@ CONTAINS
   P(PLM_INDEX(0, 0)) = 1.0_dp
 
   DO M = 0, LMAX
-      IF (M .GT. 0) &
-          P(PLM_INDEX(M, M)) = -(2*M - 1) * SOMX2 * &
-              P(PLM_INDEX(M-1, M-1))
+    IF (M .GT. 0) &
+      P(PLM_INDEX(M, M)) = -(2*M - 1) * SOMX2 * &
+        P(PLM_INDEX(M-1, M-1))
       IF (M .LT. LMAX) &
-          P(PLM_INDEX(M+1, M)) = X * (2*M + 1) * &
-              P(PLM_INDEX(M, M))
+        P(PLM_INDEX(M+1, M)) = X * (2*M + 1) * &
+            P(PLM_INDEX(M, M))
       DO L = M+2, LMAX
-          P(PLM_INDEX(L, M)) = &
-              (X*(2*L-1)*P(PLM_INDEX(L-1, M)) - &
-              (L+M-1)*P(PLM_INDEX(L-2, M))) / (L-M)
+        P(PLM_INDEX(L, M)) = &
+            (X*(2*L-1)*P(PLM_INDEX(L-1, M)) - &
+            (L+M-1)*P(PLM_INDEX(L-2, M))) / (L-M)
       END DO
   END DO
   END SUBROUTINE ASSOC_LEGENDRE_ALL
@@ -625,18 +625,18 @@ CONTAINS
   DOM = 1.0_dp - X**2
 
   DO M = 0, LMAX
-      DO L = M, LMAX
-          IF (ABS(X) .GE. 1.0_dp .OR. L .EQ. 0) THEN
-              DP_OUT(PLM_INDEX(L, M)) = 0.0_dp
-          ELSE IF (L .EQ. M) THEN
-              DP_OUT(PLM_INDEX(L, M)) = &
-                  -L * X * P(PLM_INDEX(L, M)) / DOM
-          ELSE
-              DP_OUT(PLM_INDEX(L, M)) = &
-                  ((L+M)*P(PLM_INDEX(L-1, M)) - &
-                   L*X*P(PLM_INDEX(L, M))) / DOM
-          END IF
-      END DO
+    DO L = M, LMAX
+      IF (ABS(X) .GE. 1.0_dp .OR. L .EQ. 0) THEN
+        DP_OUT(PLM_INDEX(L, M)) = 0.0_dp
+      ELSE IF (L .EQ. M) THEN
+        DP_OUT(PLM_INDEX(L, M)) = &
+            -L * X * P(PLM_INDEX(L, M)) / DOM
+      ELSE
+        DP_OUT(PLM_INDEX(L, M)) = &
+            ((L+M)*P(PLM_INDEX(L-1, M)) - &
+            L*X*P(PLM_INDEX(L, M))) / DOM
+      END IF
+    END DO
   END DO
   END SUBROUTINE DDX_ASSOC_LEGENDRE_ALL
 
@@ -728,15 +728,15 @@ CONTAINS
   CALL ASSOC_LEGENDRE_NORM_ALL(PNORM, LMAX, DCOS(THETA))
 
   DO L = 0, LMAX
-      DO M = 0, L
-          YLM(YLM_INDEX(L, M)) = &
-              PNORM(PLM_INDEX(L, M)) * EXP(j*M*PHI)
-      END DO
-      DO M = 1, L
-          SIGN_M = 1.0_dp - 2.0_dp * MOD(M, 2)
-          YLM(YLM_INDEX(L, -M)) = SIGN_M * &
-              CONJG(YLM(YLM_INDEX(L, M)))
-      END DO
+    DO M = 0, L
+        YLM(YLM_INDEX(L, M)) = &
+            PNORM(PLM_INDEX(L, M)) * EXP(j*M*PHI)
+    END DO
+    DO M = 1, L
+        SIGN_M = 1.0_dp - 2.0_dp * MOD(M, 2)
+        YLM(YLM_INDEX(L, -M)) = SIGN_M * &
+            CONJG(YLM(YLM_INDEX(L, M)))
+    END DO
   END DO
 
   DEALLOCATE(PNORM)
